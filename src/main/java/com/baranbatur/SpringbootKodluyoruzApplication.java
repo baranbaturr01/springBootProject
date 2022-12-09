187e118af84847d34a883c74659351b18c7dd854
baran <baturbarann@gmail.com>
2022-12-09 13:26:29 +0300

    Initial commit (Kodluyoruz education)

---
+++ src/main/java/com/baranbatur/SpringbootKodluyoruzApplication.java
@@ -0,0 +1,13 @@
+package com.baranbatur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKodluyoruzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootKodluyoruzApplication.class, args);
+    }
+
+}
