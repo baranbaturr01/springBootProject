bcfa1c10495e2f7870df6f827758097abcd1fb6c
baran <baturbarann@gmail.com>
2022-12-09 14:51:39 +0300

    SpringbootKodluyoruzApplication.java (exclude some dependencies)

--- src/main/java/com/baranbatur/SpringbootKodluyoruzApplication.java
+++ src/main/java/com/baranbatur/SpringbootKodluyoruzApplication.java
@@ -3,11 +3,10 @@ package com.baranbatur;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class, org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class})
 public class SpringbootKodluyoruzApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootKodluyoruzApplication.class, args);
     }
-
 }
