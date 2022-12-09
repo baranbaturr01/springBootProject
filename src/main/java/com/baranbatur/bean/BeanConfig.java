0080d3651906fd8317acab45c870a1869d5dc416
baran <baturbarann@gmail.com>
2022-12-09 15:33:06 +0300

    Bean Scope and Bean initial method is wrote

--- src/main/java/com/baranbatur/bean/BeanConfig.java
+++ src/main/java/com/baranbatur/bean/BeanConfig.java
@@ -3,12 +3,14 @@ package com.baranbatur.bean;
 import com.baranbatur.dto.BeanDto;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 
 @Configuration
 public class BeanConfig {
 
-    @Bean
+    @Bean(initMethod = "initialBeanMethod", destroyMethod = "destroyBeanMethod")
+    @Scope("singleton")
     public BeanDto beanDto() {
         return BeanDto.builder().id(0L).beanName("beanName").beanData("beanData").build();
     }
--- src/main/java/com/baranbatur/dto/BeanDto.java
+++ src/main/java/com/baranbatur/dto/BeanDto.java
@@ -15,4 +15,16 @@ public class BeanDto {
     private Long id;
     private String beanName;
     private String beanData;
+
+    //başlangıç
+    public void initialBeanMethod() {
+        log.info("BeanDto init");
+        System.out.println("BeanDto init, Bean başlamadan önce çalışır");
+    }
+
+    //son
+    public void destroyBeanMethod() {
+        log.info("BeanDto destroy");
+        System.out.println("BeanDto destroy, Bean sonlandıktan sonra çalışır");
+    }
 }
