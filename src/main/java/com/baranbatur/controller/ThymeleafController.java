24ef0c94dca9d7680feb516e9a6c78dc61751826
baran <baturbarann@gmail.com>
2022-12-09 16:33:40 +0300

    style.css and model parameters was used

--- src/main/java/com/baranbatur/controller/ThymeleafController.java
+++ src/main/java/com/baranbatur/controller/ThymeleafController.java
@@ -1,6 +1,7 @@
 package com.baranbatur.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,6 +12,17 @@ public class ThymeleafController {
     @GetMapping("/thymeleaf1")
     public String getThymeleaf() {
         return "thymeleaf1";
+    }
+
+    //http://localhost:8081/thymeleaf2
+    @GetMapping("/thymeleaf2")
+    //Model parametresi thymeleaf a göndermek istediğimiz datayı içerir.
+    public String getThymeleaf2Model(Model model) {
+
+        model.addAttribute("key_model", "Ben modelden geldim");//bu değer html sayfasına gönderilir orada yakalamk için key kullanılır
+        model.addAttribute("key_model2", "Ben modelden geldim2");//bu değer html sayfasına gönderilir orada yakalamk için key kullanılır
+
+        return "thymeleaf1"; //eğer templates altında başka bir klasör içerisindeki başka bir html sayfasına gitmek istiyorsak oradaki html sayfasının adını yazmamız yeterlidir(klasör_adı/html_sayfası). Ex. return "thymeleaf2/thymeleaf2";
     }
 
 }
