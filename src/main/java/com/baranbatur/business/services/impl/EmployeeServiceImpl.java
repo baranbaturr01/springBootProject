10383eb5ab50af8266cb730af3e4b72b25c41225
baran <baturbarann@gmail.com>
2022-12-14 16:00:15 +0300

    EmployeeController.java

--- src/main/java/com/baranbatur/business/services/impl/EmployeeServiceImpl.java
+++ src/main/java/com/baranbatur/business/services/impl/EmployeeServiceImpl.java
@@ -24,7 +24,6 @@ public class EmployeeServiceImpl implements EmployeeServices {
     /**
      * Model Mapper
      */
-    @Autowired
     private ModelMapper modelMapper;
 
     //    List
--- src/main/java/com/baranbatur/controller/EmployeeController.java
+++ src/main/java/com/baranbatur/controller/EmployeeController.java
@@ -1,9 +1,42 @@
 package com.baranbatur.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.baranbatur.business.dto.EmployeeDto;
+import com.baranbatur.business.services.EmployeeServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin
 public class EmployeeController {
+
+    @Autowired
+    EmployeeServices employeeServices;
+
+
+    @GetMapping("/employees")
+    public List<EmployeeDto> getAllEmployees() {
+        List<EmployeeDto> employeeDtoList = employeeServices.getAllEmployees();
+        return employeeDtoList;
+    }
+
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<EmployeeDto> findEmployeeById(@PathVariable Long id) {
+        return employeeServices.getEmployeeById(id);
+    }
+
+    //SAVE
+    @PostMapping("/employees")
+    public EmployeeDto saveEmployee(@RequestBody EmployeeDto employeeDto) {
+        return employeeServices.saveEmployee(employeeDto);
+    }
+
+    @PostMapping("/employees/{id}")
+    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable Long id, @RequestBody EmployeeDto employeeDto) {
+        return employeeServices.updateEmployee(id, employeeDto);
+    }
 }
