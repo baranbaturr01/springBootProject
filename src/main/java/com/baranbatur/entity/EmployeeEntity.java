a376897fe997bf16c690bd0f0a853a8c31354f4f
baran <baturbarann@gmail.com>
2022-12-14 14:04:25 +0300

    BaseEntity.java (@MappedSuperClass ,@EntityListener,@CreatedBy,@Create..) EmployeeEntity.java (@Entity , @Table @Id,@Column )

---
+++ src/main/java/com/baranbatur/entity/EmployeeEntity.java
@@ -0,0 +1,34 @@
+package com.baranbatur.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "employees")
+public class EmployeeEntity extends BaseEntity implements Serializable {
+
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "email_id")
+    private String emailId;
+
+    public EmployeeEntity(String name, String surname, String emailId) {
+        this.name = name;
+        this.surname = surname;
+        this.emailId = emailId;
+    }
+
+
+}
