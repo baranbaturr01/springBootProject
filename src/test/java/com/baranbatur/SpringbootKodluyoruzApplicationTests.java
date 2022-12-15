e845c97a6153fcdf22a661e9890421c2a5bb9842
baran <baturbarann@gmail.com>
2022-12-15 15:19:58 +0300

    unitTest created

--- src/test/java/com/baranbatur/SpringbootKodluyoruzApplicationTests.java
+++ src/test/java/com/baranbatur/SpringbootKodluyoruzApplicationTests.java
@@ -1,13 +1,72 @@
 package com.baranbatur;
 
+import com.baranbatur.data.entity.EmployeeEntity;
+import com.baranbatur.data.repository.EmployeeRepository;
+import com.baranbatur.test.TestCrud;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
-class SpringbootKodluyoruzApplicationTests {
+class SpringbootKodluyoruzApplicationTests implements TestCrud {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
 
     @Test
     void contextLoads() {
     }
 
+    @Override
+    @Test
+    public void testCreate() {
+
+        // Employee entity i builder olarak tanımladıgımız için burada builder yapısı kullanmayı tercih ettik.
+        EmployeeEntity employeeEntity = EmployeeEntity.builder().name("Baran").surname("Batur").emailId("barnabatu@gmaşl.com").build();
+
+        employeeRepository.save(employeeEntity);
+
+        assert employeeEntity.getId() != null;
+    }
+
+    @Override
+    @Test
+
+    public void testList() {
+
+        List<EmployeeEntity> list = employeeRepository.findAll();
+        assert list.size() > 0;
+    }
+
+    @Override
+    @Test
+    public void testFindById() {
+
+        EmployeeEntity employeeEntity = employeeRepository.findById(8L).get();
+        Assertions.assertEquals("Baran", employeeEntity.getName());
+    }
+
+    @Override
+    @Test
+
+    public void testUpdate() {
+
+        EmployeeEntity employeeEntity = employeeRepository.findById(8L).get();
+        employeeEntity.setName("Baran updated");
+        employeeRepository.save(employeeEntity);
+
+        Assertions.assertEquals("Baran updated", employeeEntity.getName());
+    }
+
+
+    @Override
+    @Test
+    public void testDelete() {
+
+        employeeRepository.deleteById(8L);
+        Assertions.assertFalse(employeeRepository.findById(8L).isPresent());
+    }
 }
---
+++ src/test/java/com/baranbatur/test/TestCrud.java
@@ -0,0 +1,16 @@
+package com.baranbatur.test;
+
+public interface TestCrud {
+
+    public void testCreate();
+
+    public void testList();
+
+    public void testUpdate();
+
+
+    public void testFindById();
+
+    public void testDelete();
+
+}
